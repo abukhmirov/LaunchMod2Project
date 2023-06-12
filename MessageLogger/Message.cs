ea24c18188db0ae719068a2468027ad3be032c26
abukhmirov <alexbukhmirov@gmail.com>
2023-06-12 10:45:10 -0600

    Migrated to create Db

--- MessageLogger/Message.cs
+++ MessageLogger/Message.cs
@@ -8,8 +8,10 @@ namespace MessageLogger
 {
     public class Message
     {
+        public int Id { get; set; }
         public string Content { get; private set; }
         public DateTime CreatedAt { get; private set; }
+        public User MessageUser { get; private set; }
 
         public Message(string content)
         {
